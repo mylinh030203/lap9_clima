c646efe8c684e1e15c73c9f82f4b928c7324bed1
Phan Mỹ Linh <havana03022003@gmail.com>
2024-11-18 00:34:32 +0700

    update

---
+++ lib/services/location.dart
@@ -0,0 +1,20 @@
+import 'package:geolocator/geolocator.dart';
+
+class Location {
+  double latitude = 0.0;
+  double longitude = 0.0;
+
+  Future<void> getCurrentLocation() async {
+    try {
+      // Gọi static method trực tiếp từ lớp Geolocator
+      Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.low,
+      );
+
+      latitude = position.latitude;
+      longitude = position.longitude;
+    } catch (e) {
+      print(e);
+    }
+  }
+}
---
+++ lib/services/networking.dart
@@ -0,0 +1,23 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class NetworkHelper {
+  NetworkHelper(this.url);
+
+  final String url;
+
+  Future getData() async {
+    // Chuyển đổi chuỗi URL thành đối tượng Uri
+    Uri uri = Uri.parse(url);
+
+    http.Response response = await http.get(uri);
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+
+      return jsonDecode(data);
+    } else {
+      print(response.statusCode);
+    }
+  }
+}
